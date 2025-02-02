9adc2394cd558362d2979903416505a1c2c7e801
Biggienation <alexpet5888@gmail.com>
2025-02-02 14:44:05 +0100

    Implement functionality for add product

--- src/main/java/com/example/payment/ShoppingCart.java
+++ src/main/java/com/example/payment/ShoppingCart.java
@@ -5,11 +5,17 @@ import com.example.Item;
 import java.util.ArrayList;
 
 public class ShoppingCart {
+    private ArrayList<Item> items;
+
+    public ShoppingCart() {
+        items = new ArrayList<>();
+    }
 
     public void add(Item item) {
+        items.add(item);
     }
 
     public ArrayList<Item> getItems() {
-        return null;
+        return items;
     }
 }
---
+++ src/test/java/com/example/payment/ShoppingCartTest.java
@@ -0,0 +1,22 @@
+package com.example.payment;
+
+import com.example.Item;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class ShoppingCartTest {
+    public static final Item TOMATO = new Item("Tomato", BigDecimal.valueOf(5L), 0, 10);
+
+    @Test
+    @DisplayName("Test for adding product")
+    void testAddProduct() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.add(TOMATO);
+        assertThat(shoppingCart.getItems()).contains(TOMATO);
+    }
+}
